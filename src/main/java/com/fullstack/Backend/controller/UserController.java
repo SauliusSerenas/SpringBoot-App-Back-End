efb6076d7e87c575e7826a04bc3fc0f2eda0ce25
SauliusSerenas <115420065+SauliusSerenas@users.noreply.github.com>
2023-03-29 20:10:29 +0300

    Add some mapping.

--- src/main/java/com/fullstack/Backend/controller/UserController.java
+++ src/main/java/com/fullstack/Backend/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.fullstack.Backend.controller;
 
+import com.fullstack.Backend.exception.UserNotFoundException;
 import com.fullstack.Backend.model.User;
 import com.fullstack.Backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +16,7 @@ public class UserController {
     private UserRepository userRepository;
 
     @PostMapping("/user")
-    User newUser(@RequestBody User newUser){
+    User newUser(@RequestBody User newUser) {
         return userRepository.save(newUser);
     }
 
@@ -23,4 +24,31 @@ public class UserController {
     List<User> getAllUsers() {
         return userRepository.findAll();
     }
+
+    @GetMapping("/user/{id}")
+    User getUserById(@PathVariable Long id) {
+        return userRepository.findById(id).orElseThrow(() ->
+                new UserNotFoundException(id));
+
+    }
+
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable Long id) {
+        return userRepository.findById(id).map(user -> {
+            user.setSurname(newUser.getSurname());
+            user.setName(newUser.getName());
+            user.setEmail(newUser.getEmail());
+            return userRepository.save(user);
+        }).orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    @DeleteMapping("/user/{id}")
+    String deleteUser(@PathVariable Long id) {
+        if (!userRepository.existsById(id)) {
+            throw new UserNotFoundException(id);
+        }
+        userRepository.deleteById(id);
+        return "Employee with id " + id + " has been deleted successfully.";
+    }
 }
+
